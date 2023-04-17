64e471da56de1f07f7df35fd0cfe4135c082638f
kaldrich1 <kaldrich@coreweave.com>
2023-04-17 18:38:55 -0400

    feat(postgres): add read only user to nodes db

--- migrations/ncore/001_initialize_ncore.sql
+++ migrations/ncore/001_initialize_ncore.sql
@@ -1,7 +1,15 @@
+CREATE USER read_only WITH PASSWORD '{{ env "READ_ONLY_PASSWORD" }}';
+
+GRANT CONNECT ON DATABASE nodes TO read_only;
+
+GRANT USAGE ON SCHEMA public TO read_only;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO read_only;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO read_only;
 
 CREATE TABLE nodes (
     mac_address text PRIMARY KEY CHECK (mac_address != '') NOT NULL,
-    hostname text NOT NULL CHECK (system_id != ''),
+    hostname text NOT NULL CHECK (hostname != ''),
     ip_address text NOT NULL CHECK (ip_address != ''),
     first_seen timestamp with time zone NOT NULL DEFAULT now(),
     last_seen timestamp with time zone NOT NULL DEFAULT now()
