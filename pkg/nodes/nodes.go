f0c88414824c341140ff98281bd644aa9e642676
kaldrich1 <kaldrich@coreweave.com>
2023-04-16 23:00:59 -0400

    fix(ncore): update comment and err  msg to correlate to func

--- pkg/nodes/nodes.go
+++ pkg/nodes/nodes.go
@@ -10,10 +10,10 @@ type Node struct {
 	IpAddress  string
 }
 
-// GetNodePayload returns a NodePayload for mac_address.
+// Update nodes stats by provided data payload.
 func (s *Service) UpdateNodeStats(ctx context.Context, n *Node) (*Node, error) {
 	if n.MacAddress == "" {
-		return nil, ValidationError{"missing payload macAddress"}
+		return nil, ValidationError{"Missing macAddress"}
 	}
 	return s.db.UpdateNodeStats(ctx, n)
 }
